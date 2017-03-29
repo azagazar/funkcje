d05dcfe7fef4e84bdfc7772cb397d10a96beed43
AgataZarod <a.zarod@alboalbo.com>
2017-03-29 15:26:50 +0200

    poprawa kodu

--- js/funkcje.js
+++ js/funkcje.js
@@ -10,26 +10,9 @@ function getTriangleArea(a, h) {
 
 console.log(getTriangleArea(10,6))
 
-function getTriangle1Area(a, h) {
-	if ((a > 0) && (h > 0)) {
-		return a * h / 2;
-	}
+var triangle1Area = getTriangleArea(10, 15);
+var triangle2Area = getTriangleArea(10, 0);
 
-	else {
-		return "Nieprawidłowe dane"
-	}
-}
+console.log(triangle1Area)
+console.log(triangle2Area)
 
-console.log(getTriangle1Area(10, 15))
-
-function getTriangle2Area(a, h) {
-	if ((a > 0) && (h > 0)) {
-		return a * h / 2;
-	}
-
-	else {
-		return "Nieprawidłowe dane"
-	}
-}
-
-console.log(getTriangle2Area(10, 0))
