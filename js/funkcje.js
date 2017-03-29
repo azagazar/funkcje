fc062873ea79258059a451f390b89c7d56f76faa
AgataZarod <a.zarod@alboalbo.com>
2017-03-29 11:52:11 +0200

    dodanie funkcji

--- js/funkcje.js
+++ js/funkcje.js
@@ -9,3 +9,27 @@ function getTriangleArea(a, h) {
 }
 
 console.log(getTriangleArea(10,6))
+
+function getTriangle1Area(a, h) {
+	if ((a > 0) && (h > 0)) {
+		return a * h / 2;
+	}
+
+	else {
+		return "Nieprawidłowe dane"
+	}
+}
+
+console.log(getTriangle1Area(10, 15))
+
+function getTriangle2Area(a, h) {
+	if ((a > 0) && (h > 0)) {
+		return a * h / 2;
+	}
+
+	else {
+		return "Nieprawidłowe dane"
+	}
+}
+
+console.log(getTriangle2Area(10, 0))
